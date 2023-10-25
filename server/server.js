b62ddde295965b073b5c5a73e8fedc1b4929d565
Jamie Branch <schwung988@yahoo.com>
2023-10-24 21:59:36 -0400

    cloudinary

--- server/server.js
+++ server/server.js
@@ -1,10 +1,77 @@
+
+require("dotenv/config");
 const express = require('express');
+const bodyParser = require("body-parser");
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+
+mongoose
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Database connected successfully"))
+  .catch(err => console.log(err));
+
+  mongoose.set("useCreateIndex", true, "useFindandModify", false);
+
+  //since mongoose promise is depreciated, we overide it with node's promise
+  mongoose.Promise = global.Promise;
+
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+  app.use(bodyParser.json({ limit: "50mb", extended: true }));
+  
+  //Initialize CORS middleware
+  app.uae(function(rep, res, next) {
+    res.header("Access-Control_Allow_Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Contect_Type, Accept"
+    );
+    next();
+  });
+    
+
+
+const imageFilter = function(req, file, cb) {
+  //accept image files only
+  if (!file.originalname.match(/\.jpg|jpeg|png|gif)$/i)) {
+    return cb(new Error("Only image files are accepted!"), false);
+  }
+  cb(null, true);
+};
+
+const upload = multer({ storage: storage, fileFilter: imageFilter });
+const cloudinary = require("cloudinary");
+
+cloudinary.config({
+  cloud_name: "dbindi09a",
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const ImageSchema = new Schema({
+  title: String,
+  image: String,
+  imageId: String
+});
+
+const Image = mongoose.model("Image", ImageSchema);
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { ppid } = require("process");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -35,7 +102,26 @@ const startApolloServer = async () => {
       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
     });
   });
+  
+  app.post("/add", upload.single("image"), (req,res) => {
+    cloudinary.v2.uploader.upload(req.file.path, function(err, result) {
+      if(err) {
+        req.json(err.message);
+      }
+      req.body.imageId = result.public_id;
+
+      Image.create(req.body,function(err, image) {
+        if (err) {
+          res.json(err.message);
+          return res.redirect("/");
+        }
+      });
+    })
+  });
 };
 
 startApolloServer();
+
+module.exports = app;
+module.exports = Image;
   
