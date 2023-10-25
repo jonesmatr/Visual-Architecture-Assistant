2f0ca22baeac24859057f5839dc938e4bc70dce6
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-10-25 08:38:41 -0400

    modifies user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
-const { Schema, model } = require(‘mongoose’);
-const bcrypt = require(‘bcrypt’);
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 const userSchema = new Schema(
   {
     username: {
@@ -11,7 +11,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      match: [/.+@.+\..+/, ‘Must use a valid email address’],
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
     },
     password: {
       type: String,
@@ -32,8 +32,8 @@ const userSchema = new Schema(
   }
 );
 // hash user password
-userSchema.pre(‘save’, async function (next) {
-  if (this.isNew || this.isModified(‘password’)) {
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
@@ -44,8 +44,8 @@ userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 // when we query a user, we’ll also get another field called `imageCount` with the number of saved images we have
-userSchema.virtual(‘imageCount’).get(function () {
+userSchema.virtual('imageCount').get(function () {
   return this.savedImages.length;
 });
-const User = model(‘User’, userSchema);
+const User = model('User', userSchema);
 module.exports = User;
