5ffc896868311ce15af64937b5595d4b35a53e62
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-26 15:42:02 -0400

    adjusting style adding up more class names in Layo

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -5,13 +5,24 @@ const Layout = ({ children }) => {
     return (
         <div>
             {/* Navbar */}
-            <Navbar bg="light" expand="lg">
+            <Navbar bg="light" expand="lg" className="custom-navbar">
+                {/* Logo */}
+                <Navbar.Brand href="/">
+                    <img
+                        src="/logo.png"  // Path to your logo
+                        width="96"   // Adjust width as needed
+                        height="55"  // Adjust height as needed
+                        className="d-inline-block align-top navbar-logo"
+                        alt="Your Logo"
+                    />
+                </Navbar.Brand>
+
                 <Navbar.Toggle aria-controls="navbar-nav" />
                 <Navbar.Collapse id="navbar-nav" className="justify-content-center">
                     <Nav>
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link href="#about" className="custom-nav-link">About</Nav.Link>
+                    <Nav.Link href="#portfolio" className="custom-nav-link">Portfolio</Nav.Link>
+                    <Nav.Link href="#pricing" className="custom-nav-link">Pricing</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
                 <Nav>
