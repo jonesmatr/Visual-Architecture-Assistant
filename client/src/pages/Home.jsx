b340855335054228e196c6e0f62f552981747254
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-26 11:52:08 -0400

    update Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import CarouselHome from '..components/CarouselHome'
+import Layout from '../components/Layout';
 
 const Home = () => {
   return (
-    <div>
-       <CarouselHome />
-    </div>
-  )
+      <Layout>
+          {/* Body */}
+          <Container>
+              <Row className="mt-5">
+                  <Col md={6}>
+                      <h2>Hey, this is the prompt box you can enter text here:</h2>
+                      <div className="mb-3">
+                          <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                          <FormControl as="textarea" id="exampleFormControlTextarea1" rows="3" />
+                          <Button className="mt-3 float-end">Submit</Button>
+                      </div>
+                  </Col>
+                  <Col md={6}>
+                      <img src="path_to_your_image.jpg" alt="Description" className="img-fluid" />
+                  </Col>
+              </Row>
+          </Container>
+      </Layout>
+  );
 }
+
 
 export default Home
