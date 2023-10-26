24ab470487bdbe36f5155a55c2b9902d415ad2b3
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-26 18:25:30 -0400

    changing text in index and fix styling for image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,18 +8,21 @@ const Home = () => {
             <Container>
                 {/* Body */}
                 <Row style={{ marginTop: '100px' }}>
-                    <Col md={6}>
-                        <h2>Hey, this is the prompt box you can enter text here:</h2>
-                        <div className="mb-3">
-                            <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                            <FormControl as="textarea" id="exampleFormControlTextarea1" rows="3" />
-                            <Button className="mt-3 float-end">Submit</Button>
-                        </div>
-                    </Col>
-                    <Col md={6}>
-                        <Card className="shadow-sm light-card-bg">
-                            <Card.Img variant="top" src="/front-page.png" alt="Front Page" />
-                        </Card>
+                <Col md={5} className="prompt-content prompt-content-col">
+                 <div className="mb-3 d-flex flex-column align-items-start">
+                 <label htmlFor="exampleFormControlTextarea1" className="form-label design-text">DESIGN TO REAL LIVE</label>
+                 <h2 className="describe-text mb-2">Describe<br />Renovation:</h2>
+                 <FormControl as="textarea" id="exampleFormControlTextarea1" rows="3" className="mb-2 custom-textarea" />
+                <Button className="mt-3 float-end">Submit</Button>
+    </div>
+</Col>
+
+                <Col md={{ span: 6, offset: 1 }}>
+                    <Card className="shadow-sm light-card-bg custom-card">
+                        <Card.Img variant="top" src="/front-page.png" alt="Front Page" />
+                    </Card>
+
+
                     </Col>
                 </Row>
             </Container>
