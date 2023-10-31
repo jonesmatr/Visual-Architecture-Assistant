fbfcecd1f0c45ca8fa517d0a52b8f6a67900bb30
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-31 11:55:57 -0400

    final adjust for Description on the left page

--- client/src/pages/FrontPage.jsx
+++ client/src/pages/FrontPage.jsx
@@ -13,7 +13,7 @@ const ImageGrid = () => {
         '/portrait-4.png',
         '/portrait-8.png',
         '/portrait-1.png',
-        '/portrait-2.png',
+        '/portrait-6.png',
         
     ];
 
@@ -29,26 +29,27 @@ const ImageGrid = () => {
 // Description Component
 const Description = () => (
     <div className="description">
-       <h2>Welcome to Contractor Showcase</h2>
-        <p>LEADING CONTRACTOR AGENCY</p>
+       <h1 className="main-heading">Welcome to Contractor Showcase</h1>
+
+        <h4>LEADING CONTRACTOR AGENCY</h4>
         <p>Contractor Showcase is the premier platform for showcasing top-tier contractors. For over a decade, we have been curating and presenting the best contractor profiles for various projects and assignments, both locally and internationally.</p>
 
-        <h2>WHY CHOOSE US</h2>
+        <h4>WHY CHOOSE US</h4>
         <p>Our distinction lies in our deep understanding of the contractors we represent. Every contractor on our platform is vetted and known to us personally. This intimate knowledge ensures that we align the right contractor with your specific needs, guaranteeing value and excellence for every project.</p>
 
-        <h2>HELPING YOU</h2>
+        <h4>HELPING YOU</h4>
         <p>With a combined experience of over 30 years in the industry, our team possesses the expertise and insights to guide and assist you in selecting the perfect contractor for your project. Whether you're looking for expertise in construction, renovation, or specialized projects, we have a diverse range of contractors to meet your needs.</p>
 
-        <h2>WHERE DO OUR CONTRACTORS WORK</h2>
+        <h4>WHERE DO OUR CONTRACTORS WORK</h4>
         <p>Our contractors are versatile and equipped to handle projects anywhere in the world, across diverse industries and sectors.</p>
 
-        <h2>WHO ARE OUR CONTRACTORS</h2>
+        <h4>WHO ARE OUR CONTRACTORS</h4>
         <p>Our roster boasts a wide range of professionals, from seasoned industry veterans to emerging talents. Whether you're looking for a general contractor, a specialized expert, or a consultant, we have the right match for you.</p>
 
-        <h2>WHAT DO THEY COST</h2>
+        <h4>WHAT DO THEY COST</h4>
         <p>Contractor fees vary based on their experience, expertise, and demand. Our pricing reflects the quality and skillset of our contractors, starting at competitive rates. Reach out to us to discuss your requirements, and our team will provide you with a tailored recommendation.</p>
 
-        <h2>CONTRACTOR SPOTLIGHT</h2>
+        <h4>CONTRACTOR SPOTLIGHT</h4>
         <p>We are proud sponsors of the Contractor Spotlight Awards. This biennial event recognizes and celebrates outstanding contractors who have made significant contributions to their respective fields.</p>
 
         <Link to="/pricing">
@@ -59,7 +60,7 @@ const Description = () => (
 
 const BannerDescription = () => (
     <div className="banner-description">
-        <h2>YOUR BANNER TEXT HERE</h2>
+        <h4>YOUR BANNER TEXT HERE</h4>
         <p>Additional description or details can go here.</p>
     </div>
 );
