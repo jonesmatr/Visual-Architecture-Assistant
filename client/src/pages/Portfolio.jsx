6d2325d2874e684b0acfb9f2f7ea4df54d2b514f
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-10-31 13:58:01 -0400

    Updated the Portfolio page

--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/Layout';
-import UploadImages from '../components/uploadImages';
-
-
+import ProfilePicture from '../components/ProfilePicture';
+import Bio from '../components/Bio';
+import WorkImages from '../components/WorkImages'; // Assuming you've named the component WorkImages
 
 const Portfolio = () => {
-  return (
+    // These states will ideally be populated using an API call to your backend
+    // to fetch the contractor's current profile data.
+    const [initialProfilePic, setInitialProfilePic] = useState(''); // Default URL or empty string
+    const [initialBio, setInitialBio] = useState(''); // Default bio or empty string
+    const [initialImages, setInitialImages] = useState([]); // Array of images with their descriptions
 
-  <Layout>
-    <UploadImages />
-    </Layout>
-  )
+    // useEffect to fetch the contractor's data when the component mounts
+    useEffect(() => {
+        // TODO: Fetch the contractor's profile data from your backend and update the states.
+        // This can be done using fetch, axios, or any other method you prefer.
+    }, []);
+
+    return (
+        <Layout>
+            <h1>My Profile</h1>
+
+            {/* Profile Picture Component */}
+            <ProfilePicture initialProfilePic={initialProfilePic} />
+
+            {/* Bio Component */}
+            <Bio initialBio={initialBio} />
+
+            {/* Work Images Component */}
+            <WorkImages initialImages={initialImages} />
+        </Layout>
+    );
 }
 
-export default Portfolio
+export default Portfolio;
