d23ffba3d470d2901bc4d53d248d7af2fb694031
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-10-31 18:25:55 -0400

    Added logout function for logged in users

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import Auth from '../utils/auth';
 
 const Layout = ({ children }) => {
+    const isLoggedIn = Auth.loggedIn();
+
+    const handleLogout = () => {
+        Auth.logout();
+        // If you're using react-router, you can redirect to home or login page
+        // For now, I'll just force a page reload to ensure the navbar updates.
+        window.location.assign('/');
+    };
+
     return (
         <div>
             {/* Navbar */}
@@ -20,16 +30,21 @@ const Layout = ({ children }) => {
                 <Navbar.Toggle aria-controls="navbar-nav" />
                 <Navbar.Collapse id="navbar-nav" className="justify-content-center">
                     <Nav>
-                    <Nav.Link href="about" className="custom-nav-link">About</Nav.Link>
-                    <Nav.Link href="portfolio" className="custom-nav-link">Portfolio</Nav.Link>
-                    <Nav.Link href="pricing" className="custom-nav-link">Pricing</Nav.Link>
+                        <Nav.Link href="about" className="custom-nav-link">About</Nav.Link>
+                        <Nav.Link href="portfolio" className="custom-nav-link">Portfolio</Nav.Link>
+                        <Nav.Link href="pricing" className="custom-nav-link">Pricing</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
                 <Nav className="login-signup-nav">
-                <Nav.Link href="login" className="small-nav-link">Login</Nav.Link>
-                 <Nav.Link href="signup" className="small-nav-link">Signup</Nav.Link>
+                    {isLoggedIn ? (
+                        <Nav.Link onClick={handleLogout} className="small-nav-link">Logout</Nav.Link>
+                    ) : (
+                        <>
+                            <Nav.Link href="login" className="small-nav-link">Login</Nav.Link>
+                            <Nav.Link href="signup" className="small-nav-link">Signup</Nav.Link>
+                        </>
+                    )}
                 </Nav>
-
             </Navbar>
 
             {/* Page Content */}
