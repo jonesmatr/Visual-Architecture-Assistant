367cc8ad267cb1b6236fc0006a8b60958a76dd2d
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-11-02 06:41:24 -0400

    Made the bio only visible to the specific user. No one else can see if it they sign into their portfolio

--- client/src/components/Bio.jsx
+++ client/src/components/Bio.jsx
@@ -1,24 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { useMutation } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_USER_PROFILE } from '../utils/queries';
 import { UPDATE_BIO } from '../utils/mutations';
 
-function Bio({ initialBio }) {
-    const [bio, setBio] = useState(initialBio);
+
+function Bio() {
+    const { loading, error, data } = useQuery(GET_USER_PROFILE);
+    const [bio, setBio] = useState('');
     const [isEditing, setIsEditing] = useState(false);
     const [updateBioMutation] = useMutation(UPDATE_BIO);
 
+    // Update local state when the query completes
     useEffect(() => {
-        // Retrieve from local storage on component mount
-        const savedBio = localStorage.getItem('userBio');
-        if (savedBio) {
-            setBio(savedBio);
+        if (data && data.userProfile) {
+            setBio(data.userProfile.bio);
         }
-    }, []);
-
-    useEffect(() => {
-        // Save bio to local storage whenever it changes
-        localStorage.setItem('userBio', bio);
-    }, [bio]);
+    }, [data]);
 
     const handleEditClick = () => {
         setIsEditing(true);
@@ -28,7 +25,10 @@ function Bio({ initialBio }) {
         setIsEditing(false);
         try {
             // Send the updated bio to the server to save it
-            await updateBioMutation({ variables: { bio } });
+            await updateBioMutation({
+                variables: { bio },
+                refetchQueries: [{ query: GET_USER_PROFILE }], // Refetch the user's profile to get the updated bio
+            });
             // Optionally, show a success message or handle other UI updates.
         } catch (error) {
             console.error("Error updating bio:", error);
@@ -36,12 +36,20 @@ function Bio({ initialBio }) {
         }
     };
 
+    if (loading) return 'Loading...';
+    if (error) return `Error: ${error.message}`;
+
     return (
         <div className="bio-container">
             <h2>About Me</h2>
             {isEditing ? (
                 <div>
-                    <textarea value={bio} onChange={e => setBio(e.target.value)} rows="5" cols="50"></textarea>
+                    <textarea 
+                        value={bio} 
+                        onChange={e => setBio(e.target.value)} 
+                        rows="5" 
+                        cols="50"
+                    />
                     <button onClick={handleSaveClick}>Save</button>
                 </div>
             ) : (
